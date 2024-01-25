9b535cafd306506ffc5695c6ccfefdb557089f90
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2024-01-25 20:38:29 +0200

    adding my code before the meeting

--- app/dashboard/signin/page.tsx
+++ app/dashboard/signin/page.tsx
@@ -1,4 +1,14 @@
 import SignInForm from "@/components/login/signinform"
+import Head from "next/head"
 export default function SignIn(){
-    return(<SignInForm />)
+    return(
+        <>
+        <Head>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+    />
+    {/* ... other head elements ... */}
+  </Head>
+    <SignInForm /></>)
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
