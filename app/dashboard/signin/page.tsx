88468da603acc01d3a5612c6a9cd021f7cddbeb1
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2024-01-26 00:40:16 +0200

    adding dashboard fix

--- app/dashboard/signin/page.tsx
+++ app/dashboard/signin/page.tsx
@@ -2,13 +2,5 @@ import SignInForm from "@/components/login/signinform"
 import Head from "next/head"
 export default function SignIn(){
     return(
-        <>
-        <Head>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-    />
-    {/* ... other head elements ... */}
-  </Head>
-    <SignInForm /></>)
+    <SignInForm />)
 }
