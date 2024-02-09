f8a3f95bc4d640f153373214d396f5f6df696b0b
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2024-02-09 12:26:50 +0200

    finishing the google authentication

--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -3,12 +3,36 @@ import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { FcGoogle } from "react-icons/fc";
 import { toast } from "react-toastify";
 import { db } from "../firebase"
-import { useNavigate } from "react-router-dom";
+import { useRouter } from "next/navigation";
+
 
 export default function OAuth() {
-  async function onGoogleClick() {
+    const router = useRouter();
+    async function onGoogleClick() {
+        try {
+          const auth = getAuth();
+          const provider = new GoogleAuthProvider();
+          const result = await signInWithPopup(auth, provider);
+          const user = result.user;
     
-  }
+          // check for the user
+    
+          const docRef = doc(db, "users", user.uid);
+          const docSnap = await getDoc(docRef);
+    
+          if (!docSnap.exists()) {
+            await setDoc(docRef, {
+              name: user.displayName,
+              email: user.email,
+              timestamp: serverTimestamp(),
+            });
+          }
+    
+          router.push('/dashboard');
+        } catch (error) {
+          toast.error("Could not authorize with Google");
+        }
+    }
   return (
     <button
       type="button"
