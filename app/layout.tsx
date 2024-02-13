243bab6ab7fed2225737cae03914432682858e54
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2024-02-13 19:09:15 +0200

    adding changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./globals.scss";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "./style.css";
