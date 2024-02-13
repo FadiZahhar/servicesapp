ccb4f3be7aa2018c8390dfa81f7bc3ae33c22e5f
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2024-02-13 22:57:06 +0200

    finishing the navbar of the dahsboard with scss styling.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,11 @@
 import './navbar.scss';
-
+import { IoSearchSharp } from "react-icons/io5";
+import { FaExpand } from "react-icons/fa";
+import { IoMdNotifications } from "react-icons/io";
+import { IoMdApps } from "react-icons/io";
+import { FaRegUser } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
+import logo from "@/assets/logo-blue.png";
 const Navbar = () => {
     return(<div className='navbar'>
         <div className='logo'>
@@ -7,10 +13,21 @@ const Navbar = () => {
             <span>Pro-Solutions.net</span>
         </div>
         <div className='icons'>
-            <img src="" alt="" className='icon' />
-            <img src="" alt="" className='icon' />
-            <img src="" alt="" className='icon' />
-            <img src="" alt="" className='icon' />
+        <IoSearchSharp className='icons' />
+        <IoMdApps className='icons'/>
+            <FaExpand className='icons'/>
+            <div className='notification'>
+                <IoMdNotifications className='icons'/>
+                <span>1</span>
+            </div>
+            <div className='user'>
+                <img
+                src={logo.src}
+                alt=""
+                />
+                <span>Fadi</span>
+            </div>
+            <IoMdSettings className='icons' />
         </div>
     </div>);
 }
