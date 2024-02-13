08a82defe4b9ba26fa6789e28e4b1700f39375dc
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2024-02-13 23:30:21 +0200

    finishing the footer of the dashboard

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar/Navbar';
 import './home.scss';
 import Menu from '@/components/Menu/Menu';
+import Footer from '@/components/Footer/Footer';
 const Home = () => {
     return(<div className="main">
     <Navbar />
@@ -10,8 +11,9 @@ const Home = () => {
     </div>
     <div className="contentContainer">
     </div>
+    
 </div>
-
+<Footer/>
 </div>)
 }
 
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,10 @@
 import './footer.scss';
 
 const Footer = () => {
-    return(<div className='footer'>Footer</div>)
+    return(<div className='footer'>
+        <span>pro-solutions.net</span>
+        <span>Admin Dashboard</span>
+    </div>)
 }
 
 export default Footer;
