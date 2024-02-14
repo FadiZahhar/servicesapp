bf5ee72d1d701798a6373f9d0b38d10af3d0b28b
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2024-02-14 17:43:36 +0200

    finishing the dashboard chart example

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -3,22 +3,43 @@ import BackEndLayout from '@/components/layouts/BackEndLayout';
 import './home.scss';
 import TopBox from '@/components/TopBox/TopBox';
 import ChartBox from '@/components/chartBox/ChartBox';
-import { chartBoxUser } from '@/lib/data';
+import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from '@/lib/data';
+import PieChartBox from '@/components/pieCartBox/PieChartBox';
+import BigChartBox from '@/components/bigChartBox/BigChartBox';
+import BarChartBox from '@/components/barChartBox/BarChartBox';
 
 const Home = () => {
     return(
     <BackEndLayout>
-        <div className='home'>
-            <div className='box box1'><TopBox /></div>
-            <div className='box box2'> <ChartBox {...chartBoxUser} /></div>
-            <div className='box box3'>Box3</div>
-            <div className='box box4'>Box4</div>
-            <div className='box box5'>Box5</div>
-            <div className='box box6'>Box6</div>
-            <div className='box box7'>Box7</div>
-            <div className='box box8'>Box8</div>
-            <div className='box box9'>Box9</div>
-        </div>
+            <div className="home">
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
+      <div className="box box4">
+        <PieChartBox />
+      </div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
+      <div className="box box7">
+        <BigChartBox />
+      </div>
+      <div className="box box8">
+        <BarChartBox {...barChartBoxVisit} />
+      </div>
+      <div className="box box9">
+        <BarChartBox {...barChartBoxRevenue} />
+      </div>
+    </div>
     </BackEndLayout>
     
     )
--- lib/data.ts
+++ lib/data.ts
@@ -380,3 +380,127 @@ export const userRows = [
       { name: "Sat", users: 450 },
     ],
   };
+
+  export const chartBoxProduct = {
+    color: "skyblue",
+    icon: "/productIcon.svg",
+    title: "Total Products",
+    number: "238",
+    dataKey: "products",
+    percentage: 21,
+    chartData: [
+      { name: "Sun", products: 400 },
+      { name: "Mon", products: 600 },
+      { name: "Tue", products: 500 },
+      { name: "Wed", products: 700 },
+      { name: "Thu", products: 400 },
+      { name: "Fri", products: 500 },
+      { name: "Sat", products: 450 },
+    ],
+  };
+  export const chartBoxRevenue = {
+    color: "teal",
+    icon: "/revenueIcon.svg",
+    title: "Total Revenue",
+    number: "$56.432",
+    dataKey: "revenue",
+    percentage: -12,
+    chartData: [
+      { name: "Sun", revenue: 400 },
+      { name: "Mon", revenue: 600 },
+      { name: "Tue", revenue: 500 },
+      { name: "Wed", revenue: 700 },
+      { name: "Thu", revenue: 400 },
+      { name: "Fri", revenue: 500 },
+      { name: "Sat", revenue: 450 },
+    ],
+  };
+  export const chartBoxConversion = {
+    color: "gold",
+    icon: "/conversionIcon.svg",
+    title: "Total Ratio",
+    number: "2.6",
+    dataKey: "ratio",
+    percentage: 12,
+    chartData: [
+      { name: "Sun", ratio: 400 },
+      { name: "Mon", ratio: 600 },
+      { name: "Tue", ratio: 500 },
+      { name: "Wed", ratio: 700 },
+      { name: "Thu", ratio: 400 },
+      { name: "Fri", ratio: 500 },
+      { name: "Sat", ratio: 450 },
+    ],
+  };
+  
+  export const barChartBoxRevenue = {
+    title: "Profit Earned",
+    color: "#8884d8",
+    dataKey: "profit",
+    chartData: [
+      {
+        name: "Sun",
+        profit: 4000,
+      },
+      {
+        name: "Mon",
+        profit: 3000,
+      },
+      {
+        name: "Tue",
+        profit: 2000,
+      },
+      {
+        name: "Wed",
+        profit: 2780,
+      },
+      {
+        name: "Thu",
+        profit: 1890,
+      },
+      {
+        name: "Fri",
+        profit: 2390,
+      },
+      {
+        name: "Sat",
+        profit: 3490,
+      },
+    ],
+  };
+  
+  export const barChartBoxVisit = {
+    title: "Total Visit",
+    color: "#FF8042",
+    dataKey: "visit",
+    chartData: [
+      {
+        name: "Sun",
+        visit: 4000,
+      },
+      {
+        name: "Mon",
+        visit: 3000,
+      },
+      {
+        name: "Tue",
+        visit: 2000,
+      },
+      {
+        name: "Wed",
+        visit: 2780,
+      },
+      {
+        name: "Thu",
+        visit: 1890,
+      },
+      {
+        name: "Fri",
+        visit: 2390,
+      },
+      {
+        name: "Sat",
+        visit: 3490,
+      },
+    ],
+  };
