833577b97c6521be2f6317e674ddad16190387d0
Omar Hassan Alkabouni <omar.alkabouni@gmail.com>
2024-02-18 17:48:08 +0200

    fix search

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,33 +1,33 @@
-import './navbar.scss';
+import "./navbar.scss";
 import { IoSearchSharp } from "react-icons/io5";
 import { FaExpand } from "react-icons/fa";
 import { IoMdNotifications } from "react-icons/io";
 import { IoMdApps } from "react-icons/io";
 import { FaRegUser } from "react-icons/fa";
 import { IoMdSettings } from "react-icons/io";
+import Searchbar from "@/components/searchbar/searchbar";
 import logo from "@/assets/logo-blue.png";
 const Navbar = () => {
-    return(<div className='navbar'>
-        <div className='logo'>
-            &nbsp;
+  return (
+    <div className="navbar">
+      <div className="logo">&nbsp;</div>
+
+      <div className="icons">
+        <Searchbar icon={<IoSearchSharp className="icons" />} />
+        <IoMdApps className="icons" />
+        <FaExpand className="icons" />
+        <div className="notification">
+          <IoMdNotifications className="icons" />
+          <span>1</span>
         </div>
-        <div className='icons'>
-        <IoSearchSharp className='icons' />
-        <IoMdApps className='icons'/>
-            <FaExpand className='icons'/>
-            <div className='notification'>
-                <IoMdNotifications className='icons'/>
-                <span>1</span>
-            </div>
-            <div className='user'>
-                <img
-                src={logo.src}
-                alt=""
-                />
-                <span>Fadi</span>
-            </div>
-            <IoMdSettings className='icons' />
+        <div className="user">
+          <img src={logo.src} alt="" />
+          <span>Fadi</span>
         </div>
-    </div>);
-}
+        <IoMdSettings className="icons" />
+        di
+      </div>
+    </div>
+  );
+};
 export default Navbar;
