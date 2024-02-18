449773acd5efacc769e6db1a0618b6eaa4a66b04
Omar Hassan Alkabouni <omar.alkabouni@gmail.com>
2024-02-18 12:54:42 +0200

    create dropdown function

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import './navbar.scss';
+import "./navbar.scss";
 import { IoSearchSharp } from "react-icons/io5";
 import { FaExpand } from "react-icons/fa";
 import { IoMdNotifications } from "react-icons/io";
@@ -6,28 +6,38 @@ import { IoMdApps } from "react-icons/io";
 import { FaRegUser } from "react-icons/fa";
 import { IoMdSettings } from "react-icons/io";
 import logo from "@/assets/logo-blue.png";
+import Dropdownicon from "@/components/dropdownicon/dropdownIcon";
 const Navbar = () => {
-    return(<div className='navbar'>
-        <div className='logo'>
-            &nbsp;
+  return (
+    <div className="navbar">
+      <div className="logo">&nbsp;</div>
+      <div className="icons">
+        <IoSearchSharp className="icons" />
+        
+        <IoMdApps className="icons" />
+        <FaExpand className="icons" />
+        <div className="notification">
+          <Dropdownicon
+            icon={<IoMdNotifications className="icons" />}
+            list={["omar", "test"]}
+            notification={3}
+          />
         </div>
-        <div className='icons'>
-        <IoSearchSharp className='icons' />
-        <IoMdApps className='icons'/>
-            <FaExpand className='icons'/>
-            <div className='notification'>
-                <IoMdNotifications className='icons'/>
-                <span>1</span>
-            </div>
-            <div className='user'>
-                <img
-                src={logo.src}
-                alt=""
-                />
-                <span>Fadi</span>
-            </div>
-            <IoMdSettings className='icons' />
+
+        <div className="user">
+          <img src={logo.src} alt="" />
+          <span>Fadi</span>
         </div>
-    </div>);
-}
+
+        <div className="notification">
+          <Dropdownicon
+            icon={<IoMdSettings className="icons" />}
+            list={[]}
+            notification={false}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Navbar;
---
+++ components/dropdownicon/dropdownIcon.jsx
@@ -0,0 +1,48 @@
+import "./dropdownicon.scss";
+import React, { useRef, useState } from "react";
+import {
+  IoMdSettings,
+  IoSearchSharp,
+  IoMdNotifications,
+  IoMdApps,
+  FaRegUser,
+} from "react-icons/io";
+
+const Dropdownicon = (props) => {
+  const [open, setOpen] = useState(false);
+  const menus = props.list;
+
+  return (
+    <div
+      className="subnav"
+      onClick={() => {
+        setOpen(!open);
+      }}
+    >
+      <div>
+        {props.icon}
+        {/* <IoMdSettings className="icons" /> */}
+        {props.notification && <span>{props.notification}</span>}
+      </div>
+      {open && (
+        <div className="bg-drop">
+          <ul>
+            {menus.map((menu) => (
+              <li
+                className=""
+                key={menu}
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                <a href="">{menu}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Dropdownicon;
